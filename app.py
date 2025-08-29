f116eff50d9f737aa127736a0718f47c5ca5ea37
Tim Chen <timchen@ieiworld.com>
2025-08-29 15:04:16 +0800

    chore: remove old content to prepare new app import

--- app.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from meme_gallery_app import MemeGalleryApp  # Import the new MemeGalleryApp class
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.iconbitmap('icon.png')  # Set the app icon
-    app = MemeGalleryApp(root)
-    root.mainloop()
